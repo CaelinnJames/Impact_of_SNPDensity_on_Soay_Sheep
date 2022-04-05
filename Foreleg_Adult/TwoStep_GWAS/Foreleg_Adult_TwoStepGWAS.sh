df74b74d28e43f7d92f918f8264039fd5447f43c
CaelinnJames <35686386+CaelinnJames@users.noreply.github.com>
2022-04-05 11:29:54 +0100

    Create Foreleg_Adult_TwoStepGWAS.sh

---
+++ Foreleg_Adult/TwoStep_GWAS/Foreleg_Adult_TwoStepGWAS.sh
@@ -0,0 +1,12 @@
+#!/bin/sh  
+#$ -cwd  
+#$ -l h_rt=2:00:00  
+#$ -pe sharedmem 1  
+#$ -l h_vmem=200G  
+#$ -M s1944058@sms.ed.ac.uk  
+#$ -m baes 
+. /etc/profile.d/modules.sh  
+
+module load igmm/apps/dissect/1.15.2c  
+
+dissect.mpich  --gwas --bfile ../../InputData/Plates_1to87_QC3 --pheno AdultForeLegResiduals.txt --out Foreleg/Adult_Foreleg_TwoStep_GWAS    
