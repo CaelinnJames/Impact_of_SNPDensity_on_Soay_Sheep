d98bac83af4a7e1045ec44e4daa66a510731963e
CaelinnJames <35686386+CaelinnJames@users.noreply.github.com>
2022-04-05 13:20:52 +0100

    Add files via upload

---
+++ Metacarpal_Lamb/ConditionalAnalysis/ConditionalAnalysisPrep.sh
@@ -0,0 +1,55 @@
+#!/bin/sh  
+#$ -cwd  
+#$ -l h_rt=1:00:00  
+#$ -pe sharedmem 1  
+#$ -l h_vmem=100G  
+#$ -M s1944058@sms.ed.ac.uk  
+#$ -m baes   
+
+
+. /etc/profile.d/modules.sh  
+
+module load igmm/apps/plink/1.90b4 
+module load igmm/apps/R/3.5.1
+
+R < GetSNPs.R --save
+
+plink --bfile ../../InputData/Plates_1to87_QC3 --sheep --not-chr 0,X --exclude SNPs_50K.txt --make-bed --out 50K/Conditional_50K_Geno  
+plink --bfile ../../InputData/HD_QC --sheep --not-chr X --exclude SNPs_HD.txt --make-bed --out HD/Conditional_HD_Geno  
+
+plink --bfile ../../InputData/Plates_1to87_QC3 --sheep --extract SNPs_50K.txt --keep MetacarpalLength_LambPhenotypes.txt --recodeA --out LambHLSNP 
+cut -d ' ' -f 3-6 --complement LambHLSNP.raw > LambHLSNP.txt
+sed '1d' LambHLSNP.txt > LambHLSNP2.txt 
+
+sort -k 2,2 MetacarpalLength_LambQAssoc.txt > Assoc1.txt  
+sort -k 2,2 LambHLSNP2.txt > Assoc2.txt  
+
+R <  ConditionalAnalysisPrep.R --save
+
+sort -k 2,2 MetacarpalLength_LambAssoc.txt > Assoc_50K.txt  
+sort -k 2,2 Assoc3.txt > QAssoc_50K.txt 
+
+rm LambHLSNP*
+rm Assoc2.txt
+rm Assoc1.txt
+rm Assoc3.txt
+
+plink --bfile ../../InputData/HD_QC --sheep --extract SNPs_HD.txt --keep MetacarpalLength_LambPhenotypes.txt --recodeA --out LambHLSNP
+cut -d ' ' -f 3-6 --complement LambHLSNP.raw > LambHLSNP.txt
+sed '1d' LambHLSNP.txt > LambHLSNP2.txt
+
+sort -k 2,2 MetacarpalLength_LambQAssoc.txt > Assoc1.txt
+sort -k 2,2 LambHLSNP2.txt > Assoc2.txt
+
+R <  ConditionalAnalysisPrep.R --save
+
+sort -k 2,2 MetacarpalLength_LambAssoc.txt > Assoc_HD.txt
+sort -k 2,2 Assoc3.txt > QAssoc_HD.txt
+
+rm LambHLSNP*
+rm Assoc2.txt
+rm Assoc1.txt
+rm Assoc3.txt
+
+
+
