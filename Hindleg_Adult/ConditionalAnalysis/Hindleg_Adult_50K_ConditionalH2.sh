e466bdf684c343bf62d3121f6681e99a780e29f6
CaelinnJames <35686386+CaelinnJames@users.noreply.github.com>
2022-04-05 13:02:23 +0100

    Add files via upload

---
+++ Hindleg_Adult/ConditionalAnalysis/Hindleg_Adult_50K_ConditionalH2.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#$ -cwd
+#$ -l h_rt=3:00:00
+#$ -pe sharedmem 1
+#$ -l h_vmem=100G
+#$ -M s1944058@sms.ed.ac.uk
+#$ -m baes 
+. /etc/profile.d/modules.sh
+
+module load igmm/apps/dissect/1.15.2c
+module load igmm/apps/plink/1.90b4
+
+mpirun -np 8 dissect.mpich --reml --reml-maxit 100 --bfile 50K/Conditional_50K_Geno --pheno AdultHindLegPhenotypes.txt --pheno-col 2 --blue --covar Assoc_50K.txt --qcovar QAssoc_50K.txt --random-effects AdultHindLegRandom.txt --random-effects-cols 1 2 --out 50K/AdultHindLeg_50K_Conditional_H2
+
+if [[ -f 50K/AdultHindLeg_50K_Conditional_H2.badsnps ]]
+
+then
+
+plink --bfile 50K/Conditional_50K_Geno --sheep --exclude 50K/AdultHindLeg_50K_Conditional_H2.badsnps --make-bed --out 50K/AdultHindLeg_50K_Conditional_H2
+
+mpirun -np 8 dissect.mpich --reml --reml-maxit 100 --bfile 50K/AdultHindLeg_50K_Conditional_H2 --pheno AdultHindLegPhenotypes.txt --pheno-col 2 --blue --covar Assoc_50K.txt --qcovar QAssoc_50K.txt --random-effects AdultHindLegRandom.txt --random-effects-cols 1 2 --out 50K/AdultHindLeg_50K_Conditional_H2
+
+fi
+
+  
