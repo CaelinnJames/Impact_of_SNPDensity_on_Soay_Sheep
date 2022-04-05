6b377019fd7ad77022df59826cc921d34ae433c1
CaelinnJames <35686386+CaelinnJames@users.noreply.github.com>
2022-04-05 11:39:45 +0100

    Update Ped50KScript.sh

--- Foreleg_Adult/Ped50KScript.sh
+++ Foreleg_Adult/Ped50KScript.sh
@@ -7,7 +7,7 @@
 
 sort -k 3,3 AdultForeLegPhenotypes.txt > Pheno1HD.txt
 
-awk '($2 >0)' ../../InputData/HD_QC.ped > Ped1HD.ped
+awk '($2 >0)' ../../InputData/Plates_1to87_QC3.ped > Ped1HD.ped
 sort -k 2,2 Ped1HD.ped > Ped2HD.ped
 join -1 3 -2 2 Pheno1HD.txt Ped2HD.ped > Ped3HD.ped
 cut -d ' ' -f 1,4,5 --complement Ped3HD.ped > AdultForeLegHD.ped
